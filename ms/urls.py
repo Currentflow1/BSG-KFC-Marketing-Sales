e41823afbb82ca0b83ae1538367c9b7167a452f3
lanssuay-del <lanssuay@gmail.com>
2026-08-02 22:42:50 +0800

    -loled

--- ms/urls.py
+++ ms/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('employees/', include('employees.urls')),
     path('area_prices/', include('area_prices.urls')),
+    path('customers/', include('customers.urls')),
 ]
