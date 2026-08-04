22849c1bdcb617d3cc44557d8d8ca805da2d17d8
lanssuay-del <lanssuay@gmail.com>
2026-08-04 13:25:39 +0800

    Added login, forecasting and records basic ui, admin styling

--- area_prices/admin.py
+++ area_prices/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Area
-from .models import Area_price
+from .models import AreaPrice
 
 admin.site.register(Area)
-admin.site.register(Area_price)
+admin.site.register(AreaPrice)
