1f3397c9ff38b1855fef00557a42e8570ec2e98d
lanssuay-del <lanssuay@gmail.com>
2026-08-07 16:06:01 +0800

    final changes for records

--- records/urls.py
+++ records/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.record_list, name="record_list"),
     path("<int:order_id>/", views.record_view, name="report_view"),
     path('reports/<int:order_id>/export-csv/', views.export_trip_report_csv, name='export_trip_report_csv'),
+    path("short-over/", views.short_over_matrix, name="short_over_matrix"),
+    path("short-over/export/", views.export_short_over_matrix_csv, name="export_short_over_matrix_csv"),
 ]
