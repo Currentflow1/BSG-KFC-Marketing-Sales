71d117243e6fd232931db392b5bcd47c02ef7959
lanssuay-del <lanssuay@gmail.com>
2026-08-28 14:07:48 +0800

    Changes 8/28/26

--- orders/services/__init__.py
+++ orders/services/__init__.py
@@ -2,27 +2,20 @@ from .customers import (
     add_customer_to_order,
     delete_customer_from_order,
 )
-
 from .delivery import (
     add_delivery_line,
     delete_delivery_line,
     set_delivery_order_type,
 )
-
 from .marketing import (
     sync_marketing_details,
 )
-
 from .orders import (
-    complete_order,
     delete_order,
-    reopen_order,
 )
-
 from .pricing import (
     get_area_price,
 )
-
 from .transactions import (
     add_transaction_line,
     delete_transaction_line,
